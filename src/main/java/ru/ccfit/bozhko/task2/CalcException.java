18ab50a89921b3d0fe596fdfce44108640cc4084
Marina <marina21idrisova@gmail.com>
2022-05-13 19:05:14 +0700

    123

--- src/main/java/ru/ccfit/bozhko/task2/CalcException.java
+++
@@ -1,7 +0,0 @@
-package ru.ccfit.bozhko.task2;
-
-public class CalcException extends Exception{
-    public CalcException(String message){
-        super(message);
-    }
-}
--- src/main/java/ru/ccfit/bozhko/task2/Command.java
+++
@@ -1,7 +0,0 @@
-package ru.ccfit.bozhko.task2;
-
-import java.util.List;
-
-public interface Command {
-    void calculate(Context context, List<String> parameters) throws CalcException;
-}
